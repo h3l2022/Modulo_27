da6e32db0a4593fbcfa57fc9e4f1739415bb72d3
Lucas Mario <lucasmariocontato@gmail.com>
2024-05-18 21:16:20 -0300

    commit ajuste

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,81 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { reactive } from 'vue';
+
+const estado = reactive({
+    primeiroNumero: '',
+    segundoNumero: '',
+    operacoes: {
+        soma: (a, b) => a + b,
+        subtracao: (a, b) => a - b,
+        multiplicacao: (a, b) => a * b,
+        divisao: (a, b) => (b !== 0 ? a / b : 'Divisão por zero'),
+    },
+    resultado: 0,
+});
+
+const calcularResultado = () => {
+    const { primeiroNumero, segundoNumero, operacaoMatematica } = estado;
+    estado.resultado = estado.operacoes[operacaoMatematica](parseFloat(primeiroNumero), parseFloat(segundoNumero));
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    <div class="container ">
+        <h1 class="fnt">Calculadora Aritmética VueJs</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <input type="text" v-model="estado.primeiroNumero" @input="calcularResultado" />
+        <input type="text" v-model="estado.segundoNumero" @input="calcularResultado" />
+
+        <select v-model="estado.operacaoMatematica" @change="calcularResultado">
+            <option value="soma">Soma</option>
+            <option value="subtracao">Subtração</option>
+            <option value="multiplicacao">Multiplicação</option>
+            <option value="divisao">Divisão</option>
+        </select>
+
+        <p>Resultado: {{ estado.resultado }}</p>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.container {
+  max-width: 320px;
+  margin: 0 auto;
+  padding: 20px;
+  background-color: #f4f4f4;
+  border-radius: 5px;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.fnt {
+  font-size: 1.2em;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+input {
+  margin: 10px 0;
+  padding: 8px;
+  width: 300px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  text-align: center;
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+select {
+  margin: 10px 0;
+  padding: 8px;
+  width: 320px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+}
+
+.result {
+  font-size: 1.2em;
+  margin-top: 10px;
 }
 </style>
